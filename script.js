671dc21ca50cbff2e2fc9f6c9374877696ed636a
Monalysa <monalysaklauck@gmail.com>
2023-03-03 08:32:02 -0300

    feat:play first music

--- script.js
+++ script.js
@@ -1,31 +1,48 @@
-const musicContainer = document.querySelector('.music-container')
-const playBtn = document.querySelector('#play')
-const prevBtn = document.querySelector('#prev')
-const nextBtn = document.querySelector('#next')
-const audio = document.querySelector('#audio')
-const progress = document.querySelector('.progress')
-const progressContainer = document.querySelector('.progress-container')
-const title = document.querySelector('#title')
-const cover = document.querySelector('#cover')
+const musicContainer = document.querySelector('.music-container');
+const playBtn = document.querySelector('#play');
+const prevBtn = document.querySelector('#prev');
+const nextBtn = document.querySelector('#next');
+const audio = document.querySelector('#audio');
+const progress = document.querySelector('.progress');
+const progressContainer = document.querySelector('.progress-container');
+const title = document.querySelector('#title');
+const cover = document.querySelector('#cover');
 
-const songs = ['carry']
+const songs = ['Carry on Wayward Son'];
 
-let songIndex = 2
+let songIndex = 0;
 
-loadSong(songs[songIndex])
+loadSong(songs[songIndex]);
 
 function loadSong(song) {
-  title.innerText = song
-  audio.src = `music/${song}.mp3`
-  cover.src = `images/${song}.jpg`
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
+}
+
+function playSong(){
+  musicContainer.classList.add('play');
+  playBtn.querySelector('i.fas').classList.remove('fa-play');
+  playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+  audio.play();
+}
+
+function pauseSong(){
+  musicContainer.classList.add('play');
+  playBtn.querySelector('i.fas').classList.add('fa-play');
+  playBtn.querySelector('i.fas').classList.remove('fa-pause');
+
+  audio.pause();
 }
 
 playBtn.addEventListener('click', () => {
-  const isPlaying = musicContainer.classList.contains('play')
+  const isPlaying = musicContainer.classList.contains('play');
 
   if(isPlaying){
-    pauseSong()
+    pauseSong();
   }else{
-    playSong()
+    playSong();
   }
 })
+
